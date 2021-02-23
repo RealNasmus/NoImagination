36adfaf638b9f65401e4bda3ea2476ff2566a61f
RealNasmus <Nasmus96@gmail.com>
2021-02-23 06:31:30 +0100

    Dank Meme

---
+++ main.js
@@ -0,0 +1,17 @@
+// this is copy pasted but cool function //
+// also was fun trying to get it to work :) //
+
+const searchBox = document.querySelector(".search-box");
+const navBtnContainer = document.querySelector(".nav-btn-container");
+const searchBtn = document.querySelector(".search-btn");
+const closeBtn = document.querySelector(".close-btn");
+
+searchBtn.addEventListener("click", () => {
+  searchBox.classList.add("active");
+  navBtnContainer.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  searchBox.classList.remove("active");
+  navBtnContainer.classList.remove("active");
+});
